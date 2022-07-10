caa27b238f92035cfc6d5d39f81a047b8e5a5dfc
zoharl3 <zoharl3@gmail.com>
2022-07-10 18:15:26 +1200

    .

--- runcmake2.bat
+++ runcmake2.bat
@@ -8,12 +8,13 @@ mkdir build
 cd build
 
 cmake ^
--DCMAKE_TOOLCHAIN_FILE=C:\prj-external-libs\vcpkg\scripts\buildsystems\vcpkg.cmake ^ -DVCPKG_TARGET_TRIPLET=x64-windows ^
+-DCMAKE_TOOLCHAIN_FILE=C:/prj-external-libs/vcpkg/scripts/buildsystems/vcpkg.cmake ^ -DVCPKG_TARGET_TRIPLET=x64-windows ^
 -DPYTHON_VERSION=37 ^
 -DGUI=ON ^
--DWIN_QT_PATH=c:\Qt\online\5.10.0\msvc2017_64\ ^
+-DWIN_QT_PATH=c:/Qt/online/5.10.0/msvc2017_64/ ^
 -DOPENVDB=1 ^
--DOPENVDB_ROOT=c:\Program Files\OpenVDB\ ^
+-DOPENVDB_ROOT="c:/Program Files/OpenVDB/" ^
+-DHOUDINI_ROOT="C:/Program Files/Side Effects Software/Houdini 19.0.657/" ^
 -DTBB=1 ^
 ..
 
