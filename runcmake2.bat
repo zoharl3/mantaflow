8144ed76e6ed2d02cb00af060384be9a65c16260
zoharl3 <zoharl3@gmail.com>
2022-07-10 23:17:10 +1200

    .

--- runcmake2.bat
+++ runcmake2.bat
@@ -11,12 +11,16 @@ cmake ^
 -DCMAKE_TOOLCHAIN_FILE=C:/prj-external-libs/vcpkg/scripts/buildsystems/vcpkg.cmake ^ -DVCPKG_TARGET_TRIPLET=x64-windows ^
 -DPYTHON_VERSION=37 ^
 -DGUI=ON ^
--DWIN_QT_PATH=c:/Qt/online/5.10.0/msvc2017_64/ ^
--DOPENVDB=1 ^
+-DDEBUG=ON ^
+-DDOUBLEPRECISION=ON ^
+-DPREPDEBUG=ON ^
+-DDEBUG_PYTHON_WITH_RELEASE=ON ^
+-DOPENVDB=ON ^
+-DOPENVDB_BLOSC=ON ^
 -DOPENVDB_ROOT="c:/Program Files/OpenVDB/" ^
 -DHOUDINI_ROOT="C:/Program Files/Side Effects Software/Houdini 19.0.657/" ^
--DTBB=1 ^
-..
+-DTBB=ON ^
+-Wno-dev ..
 
 rem -A x64 -T v141
 
