de41c4008b77aa03b2c57fe2e88fdcf251ee8184
zoharl3 <zoharl3@gmail.com>
2022-12-20 01:08:36 +1100

    .

--- scenes/zflip.py
+++ scenes/zflip.py
@@ -39,11 +39,11 @@ bScreenShot = 1
 # solver params
 dim = 2 # 2, 3
 part_per_cell_1d = 2 # 3, 2(default), 1
-it_max = 171 # 300, 500, 1200, 1500
-res = 20 # 17(min old band), 32, 48, 64(default), 96, 128(large)
+it_max = 200 # 300, 500, 1200, 1500
+res = 12 # 17(min old band), 32, 48, 64(default), 96, 128(large)
 
 b_fixed_vol = 1
-narrowBand = bool( 0 )
+narrowBand = bool( 1 )
 narrowBandWidth = 4 # 3(flip), 4(min fix: min distance between the two interfaces should be 2)
 
 combineBandWidth = narrowBandWidth - 1
