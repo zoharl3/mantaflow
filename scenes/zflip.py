9f4a07a7c56593549a1518f2616c5b8e2d07d8a0
zoharl3 <zoharl3@gmail.com>
2023-01-03 23:39:19 +1100

    .

--- scenes/zflip.py
+++ scenes/zflip.py
@@ -43,7 +43,7 @@ bScreenShot = 1
 dim = 2 # 2, 3
 part_per_cell_1d = 2 # 3, 2(default), 1
 it_max = 200 # 300, 500, 1200, 1500
-res = 140 # 32, 48, 64(default), 96, 128(large)
+res = 150 # 32, 48, 64(default), 96, 128(large)
 
 b_fixed_vol = 1
 narrowBand = bool( 1 )
