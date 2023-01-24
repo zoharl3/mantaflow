0c5aa4f0966193a91becd7184edda98b084bad94
zoharl3 <zoharl3@gmail.com>
2023-01-24 23:58:21 +1100

    .

--- scenes/zflip.py
+++ scenes/zflip.py
@@ -27,7 +27,7 @@ os.system( 'rm %s*.*' % out )
 os.system( 'cp %s../video.bat %s' % (out, out) )
 
 # (debug) for consistent result; for large res, the step() hangs?
-if 1:
+if 0:
     limit_to_one_core()
 
 # flags
@@ -42,8 +42,8 @@ bScreenShot = 1
 # solver params
 dim = 3 # 2, 3
 part_per_cell_1d = 2 # 3, 2(default), 1
-it_max = 6 # 300, 500, 1200, 1500
-res = 64 # 32, 48, 64(default), 96, 128(large), 256(, 512 is too large)
+it_max = 1400 # 300, 500, 1200, 1500
+res = 128 # 32, 48, 64(default), 96, 128(large), 256(, 512 is too large)
 
 b_fixed_vol = 1
 narrowBand = bool( 1 )
@@ -138,7 +138,7 @@ s2 = Solver( name='secondary', gridSize=gs2, dim=dim )
 flags2 = s2.create( FlagGrid )
 flags2.initDomain( boundaryWidth=0 ) 
 
-if 0: # breaking dam
+if 1: # breaking dam
     # my dam
     fluidbox = Box( parent=s, p0=gs*( vec3(0,0,0.3) ), p1=gs*( vec3(0.4,0.8,.7) ) ) 
 
