319642c9a06e2cfa70b9e8a77080e74c1b37e91b
zoharl3 <zoharl3@gmail.com>
2024-11-11 12:04:35 +1300

    .

--- runcmake.bat
+++ runcmake.bat
@@ -8,23 +8,19 @@ mkdir build
 cd build
 
 cmake ^
--DCMAKE_TOOLCHAIN_FILE=C:/prj-external-libs/vcpkg/scripts/buildsystems/vcpkg.cmake ^ -DVCPKG_TARGET_TRIPLET=x64-windows ^
--DPYTHON_VERSION=37 ^
+-DCMAKE_TOOLCHAIN_FILE=d:/vcpkg/scripts/buildsystems/vcpkg.cmake ^ -DVCPKG_TARGET_TRIPLET=x64-windows ^
+-DPYTHON_VERSION=311 ^
 -DGUI=ON ^
 -DDEBUG=OFF ^
 -DDOUBLEPRECISION=ON ^
 -DPREPDEBUG=ON ^
 -DDEBUG_PYTHON_WITH_RELEASE=ON ^
--DPython_LIBRARY="c:/python37/libs/python37.lib" ^
--DPython_INCLUDE_DIR="c:/python37/include/" ^
 -DOPENVDB=ON ^
 -DOPENVDB_BLOSC=ON ^
--DOPENVDB_ROOT="c:/Program Files/OpenVDB/" ^
 -DHOUDINI_ROOT="C:/Program Files/Side Effects Software/Houdini 19.0.657/" ^
 -DTBB=ON ^
 -DNUMPY=ON ^
--DMatlab_ROOT_DIR="%MATLAB_DIR%" ^
--Wno-dev -G "Visual Studio 17 2022" -T "v142" ..
+-Wno-dev -G "Visual Studio 17 2022" ..
 
 cd ..
 
